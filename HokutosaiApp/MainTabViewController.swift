742e38b1c39a8a5467a399a5314b88fe493e8054
sejour <shuka.takakuma@gmail.com>
2016-04-21 23:18:39 +0900

    [add] 各種ViewControllerの生成

--- HokutosaiApp/MainTabViewController.swift
+++ HokutosaiApp/MainTabViewController.swift
@@ -17,7 +17,23 @@ class MainTabViewController: UITabBarController {
     override init (nibName: String?, bundle: NSBundle?) {
         super.init(nibName: nibName, bundle: bundle)
         
-        var viewControllers: [UIViewController] = []
+        var viewControllers = [UIViewController]()
+        
+        let newsViewController = UINavigationController(rootViewController: NewsViewController())
+        newsViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .Favorites, tag: 0)
+        viewControllers.append(newsViewController)
+        
+        let eventsViewController = UINavigationController(rootViewController: ShopsViewController())
+        eventsViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .Favorites, tag: 1)
+        viewControllers.append(eventsViewController)
+        
+        let shopsViewController = UINavigationController(rootViewController: ShopsViewController())
+        shopsViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .Favorites, tag: 2)
+        viewControllers.append(shopsViewController)
+        
+        let exhibitionsViewController = UINavigationController(rootViewController: ShopsViewController())
+        exhibitionsViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .Favorites, tag: 3)
+        viewControllers.append(exhibitionsViewController)
         
         self.setViewControllers(viewControllers, animated: false)
         self.selectedIndex = 0
