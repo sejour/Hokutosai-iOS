ef18f5145be2f8dd5d5e483f08fcf59718fe1b1a
sejour <shuka.takakuma@gmail.com>
2016-04-21 21:37:19 +0900

    [add] MainTabViewController
    メインのビューコントローラー

---
+++ HokutosaiApp/MainTabViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainTabViewController.swift
+//  HokutosaiApp
+//
+//  Created by Shuka Takakuma on 2016/04/21.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import UIKit
+
+class MainTabViewController: UITabBarController {
+
+}
