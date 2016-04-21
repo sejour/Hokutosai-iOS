0bc49d70643d8d85a463b9d0eec05f0f8688ba43
sejour <shuka.takakuma@gmail.com>
2016-04-21 22:46:49 +0900

    [add] ViewControllers for news, shops, exhibitions and events

---
+++ HokutosaiApp/ExhibitionsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ExhibitionsViewController.swift
+//  HokutosaiApp
+//
+//  Created by Shuka Takakuma on 2016/04/21.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import UIKit
+
+class ExhibitionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ HokutosaiApp/NewsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  NewsViewController.swift
+//  HokutosaiApp
+//
+//  Created by Shuka Takakuma on 2016/04/21.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
