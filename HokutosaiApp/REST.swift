35a2af7fc4fa80797832873d94a5a078c2927a21
sejour <shuka.takakuma@gmail.com>
2016-04-23 20:02:21 +0900

    [modify] recipientの引数をHttpResponseに変更

--- HokutosaiApp/REST.swift
+++ HokutosaiApp/REST.swift
@@ -12,43 +12,39 @@ import ObjectMapper
 
 class REST {
     
-    private class func call<ModelType: Mappable, ErrorType: Mappable>(method: Alamofire.Method, url: URLStringConvertible, recipient: ((Bool, Result<AnyObject, NSError>, [ModelType]?, ErrorType?) -> Void)) {
+    private class func call<ModelType: Mappable, ErrorType: Mappable>(method: Alamofire.Method, url: URLStringConvertible, recipient: ((HttpResponse<[ModelType], ErrorType>) -> Void)) {
         Alamofire.request(method, url).responseJSON { response in
-            let result = response.result
-            
-            guard result.isSuccess else {
-                recipient(false, result, nil, nil)
+            guard response.result.isSuccess else {
+                recipient(HttpResponse<[ModelType], ErrorType>(isSuccess: false, response: response, model: nil, error: nil))
                 return
             }
             
             guard response.response?.statusCode < 400 else {
-                let error = Mapper<ErrorType>().map(result.value)
-                recipient(false, result, nil, error)
+                let error = Mapper<ErrorType>().map(response.result.value)
+                recipient(HttpResponse<[ModelType], ErrorType>(isSuccess: false, response: response, model: nil, error: error))
                 return
             }
             
-            let model = Mapper<ModelType>().mapArray(result.value)
-            recipient(model != nil, result, model, nil)
+            let model = Mapper<ModelType>().mapArray(response.result.value)
+            recipient(HttpResponse<[ModelType], ErrorType>(isSuccess: model != nil, response: response, model: model, error: nil))
         }
     }
     
-    private class func call<ModelType: Mappable, ErrorType: Mappable>(method: Alamofire.Method, url: URLStringConvertible, recipient: ((Bool, Result<AnyObject, NSError>, ModelType?, ErrorType?) -> Void)) {
+    private class func call<ModelType: Mappable, ErrorType: Mappable>(method: Alamofire.Method, url: URLStringConvertible, recipient: ((HttpResponse<ModelType, ErrorType>) -> Void)) {
         Alamofire.request(method, url).responseJSON { response in
-            let result = response.result
-            
-            guard result.isSuccess else {
-                recipient(false, result, nil, nil)
+            guard response.result.isSuccess else {
+                recipient(HttpResponse<ModelType, ErrorType>(isSuccess: false, response: response, model: nil, error: nil))
                 return
             }
             
             guard response.response?.statusCode < 400 else {
-                let error = Mapper<ErrorType>().map(result.value)
-                recipient(false, result, nil, error)
+                let error = Mapper<ErrorType>().map(response.result.value)
+                recipient(HttpResponse<ModelType, ErrorType>(isSuccess: false, response: response, model: nil, error: error))
                 return
             }
             
-            let model = Mapper<ModelType>().map(result.value)
-            recipient(model != nil, result, model, nil)
+            let model = Mapper<ModelType>().map(response.result.value)
+            recipient(HttpResponse<ModelType, ErrorType>(isSuccess: model != nil, response: response, model: model, error: nil))
         }
     }
     
