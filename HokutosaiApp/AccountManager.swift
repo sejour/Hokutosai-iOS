d97b05aac4d5208d3b043fb6e398c9284fc157d9
sejour <shuka.takakuma@gmail.com>
2016-04-24 02:04:32 +0900

    [add] AccountManager
    オートログインによるアカウントを管理するため

---
+++ HokutosaiApp/AccountManager.swift
@@ -0,0 +1,10 @@
+//
+//  AccountManager.swift
+//  HokutosaiApp
+//
+//  Created by Shuka Takakuma on 2016/04/24.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import Foundation
+
