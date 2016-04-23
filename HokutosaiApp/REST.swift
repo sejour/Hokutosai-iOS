768e69919a9feed4492a881bfe0c2bf3f585d465
sejour <shuka.takakuma@gmail.com>
2016-04-23 18:46:12 +0900

    [add] REST API Clientを表すRESTクラスを追加

---
+++ HokutosaiApp/REST.swift
@@ -0,0 +1,17 @@
+//
+//  REST.swift
+//  HokutosaiApp
+//
+//  Created by Shuka Takakuma on 2016/04/23.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import ObjectMapper
+
+class REST {
+    
+    
+    
+}
