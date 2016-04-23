53142f17bc21c9e99b2c1dcc70aed069f792d748
sejour <shuka.takakuma@gmail.com>
2016-04-23 19:22:07 +0900

    [implement] REST API を呼び出す共通化メソッドを実装

--- HokutosaiApp/REST.swift
+++ HokutosaiApp/REST.swift
@@ -12,6 +12,44 @@ import ObjectMapper
 
 class REST {
     
+    private class func call<ModelType: Mappable, ErrorType: Mappable>(method: Alamofire.Method, url: URLStringConvertible, recipient: ((Bool, Result<AnyObject, NSError>, [ModelType]?, ErrorType?) -> Void)) {
+        Alamofire.request(method, url).responseJSON { response in
+            let result = response.result
+            
+            guard result.isSuccess else {
+                recipient(false, result, nil, nil)
+                return
+            }
+            
+            guard response.response?.statusCode < 400 else {
+                let error = Mapper<ErrorType>().map(result.value)
+                recipient(false, result, nil, error)
+                return
+            }
+            
+            let model = Mapper<ModelType>().mapArray(result.value)
+            recipient(model != nil, result, model, nil)
+        }
+    }
     
+    private class func call<ModelType: Mappable, ErrorType: Mappable>(method: Alamofire.Method, url: URLStringConvertible, recipient: ((Bool, Result<AnyObject, NSError>, ModelType?, ErrorType?) -> Void)) {
+        Alamofire.request(method, url).responseJSON { response in
+            let result = response.result
+            
+            guard result.isSuccess else {
+                recipient(false, result, nil, nil)
+                return
+            }
+            
+            guard response.response?.statusCode < 400 else {
+                let error = Mapper<ErrorType>().map(result.value)
+                recipient(false, result, nil, error)
+                return
+            }
+            
+            let model = Mapper<ModelType>().map(result.value)
+            recipient(model != nil, result, model, nil)
+        }
+    }
     
 }
