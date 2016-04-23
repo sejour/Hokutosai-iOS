cf0c530f2104e34738621166362af5337fc17d50
sejour <shuka.takakuma@gmail.com>
2016-04-23 23:16:30 +0900

    [add] HokutosaiApiクラス
    HokutosaiApiを呼び出すためAPIクライントコントローラ

---
+++ HokutosaiApp/HokutosaiApi.swift
@@ -0,0 +1,18 @@
+//
+//  HokutosaiApi.swift
+//  HokutosaiApp
+//
+//  Created by Shuka Takakuma on 2016/04/23.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import ObjectMapper
+
+class HokutosaiApi {
+    
+    private static let apiUserId = "client-ios-app"
+    private static let apiAccessToken = "hJixYqBgFOMgOvvXy8pbZ84JrNfuV8a2PKaMedhvFKj87AKGPtla19HKRKqsPya3"
+    
+}
