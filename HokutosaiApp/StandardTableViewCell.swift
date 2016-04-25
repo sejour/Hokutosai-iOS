32673099a66b62451c1283ee28df49fb13012c12
sejour <shuka.takakuma@gmail.com>
2016-04-25 23:54:05 +0900

    [implement] StandardTableViewCell

--- HokutosaiApp/StandardTableViewCell.swift
+++ HokutosaiApp/StandardTableViewCell.swift
@@ -10,15 +10,30 @@ import UIKit
 
 class StandardTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var displayedImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var organizerLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        self.preservesSuperviewLayoutMargins = false
+        self.layoutMargins = UIEdgeInsetsZero
+        self.separatorInset = UIEdgeInsetsZero
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func updateData(data: StandardTableViewCellData) {
+        self.titleLabel.text = data.dataTitle
+        self.organizerLabel.text = data.dataOrganizer
+        self.descriptionLabel.text = data.dataDescription
     }
     
 }
