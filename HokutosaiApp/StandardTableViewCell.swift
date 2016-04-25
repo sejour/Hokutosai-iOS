aff8b4adca0f863f0f4cc6401030757263b5cdfd
Shuka Takakuma <shuka.takakuma@gmail.com>
2016-04-25 13:55:54 +0900

    [add] StandardTableViewCell
    模擬店と展示で共通のテーブルビューセルを実装するため

---
+++ HokutosaiApp/StandardTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  StandardTableViewCell.swift
+//  HokutosaiApp
+//
+//  Created by Shuka Takakuma on 2016/04/25.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import UIKit
+
+class StandardTableViewCell: UITableViewCell {
+
+    
+    
+}
