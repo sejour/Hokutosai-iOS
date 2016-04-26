c7d50d85511a1c98f7a5263d17acf973ce3753da
sejour <shuka.takakuma@gmail.com>
2016-04-26 21:16:11 +0900

    [modify] 背景らカラーを白に設定

--- HokutosaiApp/StandardDetailsViewController.swift
+++ HokutosaiApp/StandardDetailsViewController.swift
@@ -13,7 +13,7 @@ class StandardDetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.whiteColor()
     }
 
     override func didReceiveMemoryWarning() {
