40038941560bc076876adb4f406ac72cb3b3dde0
sejour <shuka.takakuma@gmail.com>
2016-04-28 15:00:28 +0900

    [add] StandardTableViewCellDelegate

---
+++ HokutosaiApp/StandardTableViewCellDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  StandardTableViewCellDelegate.swift
+//  HokutosaiApp
+//
+//  Created by Shuka Takakuma on 2016/04/28.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import Foundation
+
+protocol StandardTableViewCellDelegate {
+    
+    func like(data: StandardTableViewCellData)
+    
+}
