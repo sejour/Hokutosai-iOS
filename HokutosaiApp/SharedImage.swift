14dd3a0f1a0507def7aae9a4a16e27d707ad7b75
sejour <shuka.takakuma@gmail.com>
2016-04-28 17:58:53 +0900

    [modify] SharedImageをfinalクラスに変更

--- HokutosaiApp/SharedImage.swift
+++ HokutosaiApp/SharedImage.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class SharedImage {
+final class SharedImage {
     
     static let noImage = UIImage(named: "NoImage")
     static let grayHertIcon = UIImage(named: "GrayHertIcon")
     static let redHertIcon = UIImage(named: "RedHertIcon")
+    
+    private init () {}
     
 }
