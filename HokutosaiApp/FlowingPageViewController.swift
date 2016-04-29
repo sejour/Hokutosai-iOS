8ffb3c90b32cd889d9a56bfc1d3b1ad9a0a9065b
sejour <shuka.takakuma@gmail.com>
2016-04-29 20:23:51 +0900

    [implement] UIPageViewControllerDataSourceメソッドをオーバーライド

--- HokutosaiApp/FlowingPageViewController.swift
+++ HokutosaiApp/FlowingPageViewController.swift
@@ -21,15 +21,26 @@ class FlowingPageViewController: SlidePageViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        
+        guard let index = self.currentPageNumber else {
+            return nil
+        }
+        
+        let nextIndex = index <= 0 ? self.pageCount - 1 : index - 1
+        
+        return self.pages[nextIndex]
     }
-    */
+    
+    override func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        guard let index = self.currentPageNumber else {
+            return nil
+        }
+        
+        let nextIndex = index >= self.pageCount - 1 ? 0 : index + 1
+        
+        return self.pages[nextIndex]
+    }
 
 }
