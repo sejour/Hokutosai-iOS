b8c5f1c6376f4663a3d6a0c92e11371c529ebbaf
sejour <shuka.takakuma@gmail.com>
2016-04-30 03:03:06 +0900

    [implement] Topicコンテンツの生成を実装

--- HokutosaiApp/NewsViewController.swift
+++ HokutosaiApp/NewsViewController.swift
@@ -8,17 +8,59 @@
 
 import UIKit
 
-class NewsViewController: UIViewController {
-
+class NewsViewController: UIViewController, TappableViewControllerDelegate {
+    
+    var topics: [TopicNews]!
+    
+    var topicsBordController: FlowingPageViewController!
+    
+    let whRatio: CGFloat = 2.0 / 5.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.title = "特集"
+        self.view.backgroundColor = UIColor.whiteColor()
+        
+        self.generateTopics()
+    }
+    
+    private func generateTopics() {
+        self.topicsBordController = FlowingPageViewController()
+        self.topicsBordController.registParentViewController(self)
+        self.topicsBordController.viewOrigin = CGPoint(x: 0.0, y: self.appearOriginY)
+        self.topicsBordController.viewSize = CGSize(width: self.view.frame.size.width, height: self.whRatio * self.view.frame.size.width)
+        self.topicsBordController.view.backgroundColor = UIColor.whiteColor()
+        
+        HokutosaiApi.GET(HokutosaiApi.News.Topics()) { response in
+            guard response.isSuccess else {
+                self.presentViewController(ErrorAlert.Server.failureGet(), animated: true, completion: nil)
+                return
+            }
+            
+            self.topics = response.model
+            
+            var pages = [TopicViewController]()
+            for i in 0 ..< self.topics.count {
+                let topicViewController = TopicViewController()
+                topicViewController.view.frame = CGRect(x: 0.0, y: 0.0, width: self.topicsBordController.viewSize.width, height: self.topicsBordController.viewSize.height)
+                topicViewController.setTopicContentData(i, data: self.topics[i])
+                topicViewController.delegate = self
+                pages.append(topicViewController)
+            }
+            
+            self.topicsBordController.pages = pages
+            self.topicsBordController.startFlowing()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tappedView(sender: TappableViewController, gesture: UITapGestureRecognizer, tag: Int) {
+        print(tag)
     }
 
 }
