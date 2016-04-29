c05a6ebe6f667ce5442123606ae1123199d66bc7
sejour <shuka.takakuma@gmail.com>
2016-04-30 03:20:09 +0900

    [add] Article model

---
+++ HokutosaiApp/Article.swift
@@ -0,0 +1,20 @@
+//
+//  Article.swift
+//  HokutosaiApp
+//
+//  Created by Shuka Takakuma on 2016/04/30.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class Article: Mappable {
+    
+    required init?(_ map: Map) { }
+    
+    func mapping(map: Map) {
+        
+    }
+    
+}
