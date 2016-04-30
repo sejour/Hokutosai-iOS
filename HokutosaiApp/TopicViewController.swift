fb435bc49273a70f138258d741d8586f81db5ba8
sejour <shuka.takakuma@gmail.com>
2016-05-01 05:18:00 +0900

    [add] TappableViewControllerにコンテントモードとクリッピングを設定

--- HokutosaiApp/TopicViewController.swift
+++ HokutosaiApp/TopicViewController.swift
@@ -19,6 +19,8 @@ class TopicViewController: TappableViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.imageView.contentMode = .ScaleAspectFill
+        self.imageView.clipsToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
