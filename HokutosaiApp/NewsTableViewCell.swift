b7db828158ae93f0f16d0806e040e371313b4a80
sejour <shuka.takakuma@gmail.com>
2016-04-30 23:39:33 +0900

    [implement] NewsTableViewCellを実装

--- HokutosaiApp/NewsTableViewCell.swift
+++ HokutosaiApp/NewsTableViewCell.swift
@@ -7,10 +7,11 @@
 //
 
 import UIKit
+import OAStackView
 import SnapKit
 
 class NewsTableViewCell: UITableViewCell {
-
+    
     var firstImageView: UIImageView!
     var newsContentView: UIView!
     
@@ -20,14 +21,28 @@ class NewsTableViewCell: UITableViewCell {
     @IBOutlet weak var likeButton: UIButton!
     @IBOutlet weak var likesCountLabel: UILabel!
     
+    var index: Int!
+    var data: Article!
+    
+    private static let contentHeight: CGFloat = 80.0
+    static let rowHeight: CGFloat = 81.0
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        self.preservesSuperviewLayoutMargins = false
+        self.layoutMargins = UIEdgeInsetsZero
+        self.separatorInset = UIEdgeInsetsZero
+        
         self.firstImageView = UIImageView()
         self.contentView.addSubview(self.firstImageView)
+        self.firstImageView.hidden = true
+        self.firstImageView.contentMode = .ScaleAspectFill
+        self.firstImageView.clipsToBounds = true
         
         self.newsContentView = NSBundle.mainBundle().loadNibNamed("NewsTableViewCellContent", owner: self, options: nil).first as! UIView
         self.contentView.addSubview(self.newsContentView)
+        self.newsContentView.hidden = true
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -38,11 +53,80 @@ class NewsTableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
-
+    
+    func changeData(index: Int, article: Article) {
+        self.data = article
+        self.index = index
+        
+        self.initLayout()
+        
+        self.titleLabel.text = article.title ?? "タイトル無し"
+        
+        if let relatedEvent = article.relatedEvent, let organizer = relatedEvent.title {
+            self.titleLabel.text = organizer
+        }
+        else if let relatedShop = article.relatedShop, let organizer = relatedShop.name {
+            self.titleLabel.text = organizer
+        }
+        else if let relatedExhibition = article.relatedExhibition, let organizer = relatedExhibition.title {
+            self.titleLabel.text = organizer
+        }
+        else {
+            self.organizerLabel.text = nil
+        }
+        
+        if let datetime = article.datetime {
+            self.datetimeLabel.text = NSDate.stringFromDate(datetime, format: "yyyy-MM-dd HH:mm")
+        }
+        else {
+            self.datetimeLabel.text = nil
+        }
+        
+        if let likesCount = article.likesCount {
+            self.likesCountLabel.text = String(likesCount)
+        }
+        else {
+            self.likesCountLabel.text = "0"
+        }
+        
+        if let liked = article.liked where liked {
+            self.likeButton.imageView?.image = SharedImage.redHertIcon
+            self.likesCountLabel.textColor = SharedColor.likesCountRed
+        }
+        else {
+            self.likeButton.imageView?.image = SharedImage.grayHertIcon
+            self.likesCountLabel.textColor = SharedColor.likesCountGray
+        }
+        
+        self.firstImageView.image = nil
+        var contentViewLeft = self.contentView.snp_left
+        if let imageUrl = article.medias?.first?.url, let url = NSURL(string: imageUrl) {
+            contentViewLeft = self.firstImageView.snp_right
+            self.firstImageView.hidden = false
+            self.firstImageView.af_setImageWithURL(url)
+            self.firstImageView.snp_makeConstraints { make in
+                make.left.equalTo(self.contentView)
+                make.top.equalTo(self.contentView)
+                make.width.height.equalTo(NewsTableViewCell.contentHeight)
+            }
+        }
+        
+        self.newsContentView.snp_makeConstraints { make in
+            make.left.equalTo(contentViewLeft)
+            make.right.equalTo(self.contentView)
+            make.top.equalTo(self.contentView)
+            make.height.equalTo(NewsTableViewCell.contentHeight)
+        }
+    }
+    
+    private func initLayout() {
+        self.firstImageView.hidden = true
+        self.newsContentView.hidden = false
+        self.firstImageView.snp_removeConstraints()
+        self.newsContentView.snp_removeConstraints()
+    }
 }
