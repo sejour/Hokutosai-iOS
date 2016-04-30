914f730436dc0efed41c9a4c0a0fdd1766e2d3ed
sejour <shuka.takakuma@gmail.com>
2016-04-30 18:18:43 +0900

    [add] NewsTableViewCell

---
+++ HokutosaiApp/NewsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsTableViewCell.swift
+//  HokutosaiApp
+//
+//  Created by Shuka Takakuma on 2016/04/30.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
