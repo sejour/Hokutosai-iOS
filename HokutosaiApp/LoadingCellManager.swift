adc0c5298ad977681bbb09158b18b8d8e3cf5224
sejour <shuka.takakuma@gmail.com>
2016-05-01 20:16:20 +0900

    [add] LoadingCellManager

---
+++ HokutosaiApp/LoadingCellManager.swift
@@ -0,0 +1,15 @@
+//
+//  LoadingCellManager.swift
+//  HokutosaiApp
+//
+//  Created by Shuka Takakuma on 2016/05/01.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import UIKit
+
+class LoadingCellManager {
+    
+    let cell = UITableViewCell()
+    
+}
