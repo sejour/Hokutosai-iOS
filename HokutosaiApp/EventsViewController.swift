4f612ac1e585f26e97e070b37ee020014d6c22ac
sejour <shuka.takakuma@gmail.com>
2016-05-02 04:05:04 +0900

    [add] TopicEventの表示を実装

--- HokutosaiApp/EventsViewController.swift
+++ HokutosaiApp/EventsViewController.swift
@@ -8,12 +8,31 @@
 
 import UIKit
 
-class EventsViewController: UIViewController {
+class EventsViewController: UIViewController, TappableViewControllerDelegate {
+    
+    private var topics: [TopicEvent]!
+    private var topicsBordController: FlowingPageViewController!
+    private let topicsBordWidthHeightRatio: CGFloat = 2.0 / 5.0
+    
+    private var updatingTopics: Bool = false
+    private var updatingEvents: Bool = false
+    var updatingContents: Bool { return self.updatingTopics || self.updatingEvents }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.title = "企画"
+        self.view.backgroundColor = UIColor.whiteColor()
+        
+        self.generateTopics()
+        
+        let loadingView = SimpleLoadingView(frame: self.view.frame)
+        self.view.addSubview(loadingView)
+        self.updateContents() {
+            if !self.updatingContents {
+                loadingView.removeFromSuperview()
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,4 +40,54 @@ class EventsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    private func generateTopics() {
+        self.topicsBordController = FlowingPageViewController()
+        self.topicsBordController.registParentViewController(self)
+        self.topicsBordController.viewOrigin = CGPoint(x: 0.0, y: self.appearOriginY)
+        self.topicsBordController.viewSize = CGSize(width: self.view.frame.size.width, height: self.topicsBordWidthHeightRatio * self.view.frame.size.width)
+        self.topicsBordController.view.backgroundColor = UIColor.whiteColor()
+        
+        let bottomLine = UIView(frame: CGRect(x: 0, y: self.topicsBordController.viewSize.height - 0.5, width: self.topicsBordController.viewSize.width, height: UIViewController.pixelWidth))
+        bottomLine.backgroundColor = UIColor.grayscale(0, alpha: 80)
+        self.topicsBordController.view.addSubview(bottomLine)
+    }
+    
+    private func updateTopics(completion: (() -> Void)? = nil) {
+        guard !self.updatingTopics else { return }
+        self.updatingTopics = true
+        
+        HokutosaiApi.GET(HokutosaiApi.Events.Topics()) { response in
+            guard response.isSuccess else {
+                self.updatingTopics = false
+                completion?()
+                return
+            }
+            
+            self.topics = response.model
+            
+            var pages = [TopicViewController]()
+            for i in 0 ..< self.topics.count {
+                let topicViewController = TopicViewController()
+                topicViewController.view.frame = CGRect(x: 0.0, y: 0.0, width: self.topicsBordController.viewSize.width, height: self.topicsBordController.viewSize.height)
+                topicViewController.setTopicContentData(i, data: self.topics[i])
+                topicViewController.delegate = self
+                pages.append(topicViewController)
+            }
+            
+            self.topicsBordController.stopFlowing()
+            self.topicsBordController.pages = pages
+            self.updatingTopics = false
+            completion?()
+            self.topicsBordController.startFlowing()
+        }
+    }
+
+    private func updateContents(completion: () -> Void) {
+        self.updateTopics(completion)
+    }
+    
+    func tappedView(sender: TappableViewController, gesture: UITapGestureRecognizer, tag: Int) {
+        print(tag)
+    }
+    
 }
