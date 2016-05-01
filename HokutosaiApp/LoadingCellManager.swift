d0b9a817b70231b906e2bd7555cec04aa31cd1e6
sejour <shuka.takakuma@gmail.com>
2016-05-01 21:38:20 +0900

    [implement] LoadinCellManager

--- HokutosaiApp/LoadingCellManager.swift
+++ HokutosaiApp/LoadingCellManager.swift
@@ -8,8 +8,49 @@
 
 import UIKit
 
+enum LoadingCellStatus {
+    case Loading
+    case ReadyReload
+}
+
 class LoadingCellManager {
     
-    let cell = UITableViewCell()
+    let cell: UITableViewCell
+    private let loadingView: SimpleLoadingView
+    private let readyReloadView: UILabel
+    
+    init(size: CGSize, backgroundColor: UIColor, textColor: UIColor, textForReadyReload: String) {
+        self.cell = UITableViewCell()
+        self.cell.separatorInset = UIEdgeInsetsMake(0, CGFloat(UInt16.max), 0, 0)
+        
+        let frame = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        
+        self.loadingView = SimpleLoadingView(frame: frame, backgroundColor: backgroundColor)
+        
+        self.readyReloadView = UILabel(frame: frame)
+        self.readyReloadView.backgroundColor = backgroundColor
+        self.readyReloadView.textColor = textColor
+        self.readyReloadView.textAlignment = .Center
+        self.readyReloadView.font = UIFont.systemFontOfSize(18.0)
+        self.readyReloadView.text = textForReadyReload
+        
+        self.cell.selectionStyle = .None
+        self.cell.contentView.addSubview(loadingView)
+    }
+    
+    var status: LoadingCellStatus = .Loading {
+        didSet {
+            switch self.status {
+            case .ReadyReload:
+                self.cell.selectionStyle = .Default
+                self.loadingView.removeFromSuperview()
+                self.cell.contentView.addSubview(self.readyReloadView)
+            default:
+                self.cell.selectionStyle = .None
+                self.readyReloadView.removeFromSuperview()
+                self.cell.contentView.addSubview(self.loadingView)
+            }
+        }
+    }
     
 }
