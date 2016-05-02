a2e3f070df39debe8371fb8655e923fc0746a3dd
sejour <shuka.takakuma@gmail.com>
2016-05-02 17:02:08 +0900

    [add] EventsTableViewCell

---
+++ HokutosaiApp/EventsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventsTableViewCell.swift
+//  HokutosaiApp
+//
+//  Created by Shuka Takakuma on 2016/05/02.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import UIKit
+
+class EventsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
