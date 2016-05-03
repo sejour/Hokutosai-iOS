337dd66cf83a28cadad4c6bc002d4117ab3e5ee6
sejour <shuka.takakuma@gmail.com>
2016-05-03 22:33:07 +0900

    [implement] LikeableTableViewCellDelegate

--- HokutosaiApp/EventsTimetableViewController.swift
+++ HokutosaiApp/EventsTimetableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class EventsTimetableViewController: UITableViewController {
+class EventsTimetableViewController: UITableViewController, LikeableTableViewCellDelegate {
 
     let cellIdentifier = "Events"
     
@@ -41,7 +41,9 @@ class EventsTimetableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
@@ -58,54 +60,44 @@ class EventsTimetableViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(self.cellIdentifier, forIndexPath: indexPath) as! EventsTableViewCell
 
+        cell.delegate = self
         cell.changeData(indexPath.row, data: self.timetable![indexPath.row])
 
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    func like(index: Int, cell: LikeableTableViewCell) {
+        guard self.timetable != nil else { return }
+        
+        let eventId = self.timetable![index].eventId!
+        HokutosaiApi.POST(HokutosaiApi.Events.Likes(eventId: eventId)) { response in
+            guard let result = response.model else {
+                self.presentViewController(ErrorAlert.Server.failureSendRequest(), animated: true, completion: nil)
+                cell.updateLikes(eventId)
+                return
+            }
+            
+            self.timetable![index].liked = result.liked
+            self.timetable![index].likesCount = result.likesCount
+            cell.updateLikes(eventId)
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func dislike(index: Int, cell: LikeableTableViewCell) {
+        guard self.timetable != nil else { return }
+        
+        let eventId = self.timetable![index].eventId!
+        HokutosaiApi.DELETE(HokutosaiApi.Events.Likes(eventId: eventId)) { response in
+            guard let result = response.model else {
+                self.presentViewController(ErrorAlert.Server.failureSendRequest(), animated: true, completion: nil)
+                cell.updateLikes(eventId)
+                return
+            }
+            
+            self.timetable![index].liked = result.liked
+            self.timetable![index].likesCount = result.likesCount
+            cell.updateLikes(eventId)
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
