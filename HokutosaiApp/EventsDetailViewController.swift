30e79d5bded1672f00f1014b66960ebd138f9867
sejour <shuka.takakuma@gmail.com>
2016-05-04 21:56:19 +0900

    [add] EventsDetailViewController

---
+++ HokutosaiApp/EventsDetailViewController.swift
@@ -0,0 +1,62 @@
+//
+//  EventsDetailViewController.swift
+//  HokutosaiApp
+//
+//  Created by Shuka Takakuma on 2016/05/04.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import UIKit
+
+class EventsDetailViewController: UITableViewController {
+
+    var event: Event!
+    
+    init (event: Event) {
+        super.init(nibName: nil, bundle: NSBundle.mainBundle())
+        self.event = event
+        self.title = event.title
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+}
