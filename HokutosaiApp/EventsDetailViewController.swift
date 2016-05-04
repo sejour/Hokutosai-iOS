7f30bb3d44558df99d147cce84c97c31697111f2
sejour <shuka.takakuma@gmail.com>
2016-05-05 05:28:59 +0900

    [add] ContentsViewContrllerを継承

--- HokutosaiApp/EventsDetailViewController.swift
+++ HokutosaiApp/EventsDetailViewController.swift
@@ -8,12 +8,12 @@
 
 import UIKit
 
-class EventsDetailViewController: UITableViewController {
+class EventsDetailViewController: ContentsViewController {
 
     var event: Event!
     
     init (event: Event) {
-        super.init(nibName: nil, bundle: NSBundle.mainBundle())
+        super.init(title: event.title)
         self.event = event
         self.title = event.title
     }
@@ -24,39 +24,10 @@ class EventsDetailViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
 
 }
