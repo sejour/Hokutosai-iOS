418b3d42e9141ca9e4a14a74c001511537de5657
sejour <shuka.takakuma@gmail.com>
2016-05-04 18:40:09 +0900

    [rename] StarIcon -> FeatureIcon

--- HokutosaiApp/SharedImage.swift
+++ HokutosaiApp/SharedImage.swift
@@ -14,7 +14,7 @@ final class SharedImage {
     static let noImageWide = UIImage(named: "NoImageWide")
     static let grayHertIcon = UIImage(named: "GrayHertIcon")
     static let redHertIcon = UIImage(named: "RedHertIcon")
-    static let starIcon = UIImage(named: "StarIcon")
+    static let featureIcon = UIImage(named: "FeatureIcon")
     
     private init () {}
     
