ac0f94928effef3a00dd08abb7dfe2d96b1b6c40
sejour <shuka.takakuma@gmail.com>
2016-05-05 05:47:37 +0900

    [add] TitleView

---
+++ HokutosaiApp/TitleView.swift
@@ -0,0 +1,15 @@
+//
+//  TitleView.swift
+//  HokutosaiApp
+//
+//  Created by Shuka Takakuma on 2016/05/05.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import UIKit
+
+class TitleView: UIView {
+
+    
+
+}
