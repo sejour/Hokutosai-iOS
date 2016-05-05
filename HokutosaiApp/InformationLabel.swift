abeaa3df9451d9bea1cd4ae63415ff0f6c096ac9
sejour <shuka.takakuma@gmail.com>
2016-05-05 23:08:22 +0900

    [fix] InformationLabelの実装を修正

--- HokutosaiApp/InformationLabel.swift
+++ HokutosaiApp/InformationLabel.swift
@@ -10,38 +10,44 @@ import UIKit
 import SnapKit
 
 class InformationLabel: UIView {
-
-    var iconView: UIImageView!
-    var label: UILabel!
     
-    convenience init(width: CGFloat, icon: UIImage?, text: String?) {
+    convenience init(width: CGFloat, icon: UIImage?, text: String?, iconSize: CGFloat = 25.0) {
         self.init(frame: CGRect(x: 0.0, y: 0.0, width: width, height: 0.0), icon: icon, text: text)
     }
     
-    init(frame: CGRect, icon: UIImage?, text: String?) {
+    init(frame: CGRect, icon: UIImage?, text: String?, iconSize: CGFloat = 25.0) {
         super.init(frame: frame)
     
-        self.iconView = UIImageView(image: icon)
-        self.iconView.contentMode = .ScaleAspectFit
-        self.addSubview(self.iconView)
-        self.iconView.snp_makeConstraints { make in
-            make.width.equalTo(25.0)
-            make.height.equalTo(25.0)
+        let iconView = UIImageView(image: icon)
+        iconView.contentMode = .ScaleAspectFit
+        addSubview(iconView)
+        iconView.snp_makeConstraints { make in
+            make.width.equalTo(iconSize)
+            make.height.equalTo(iconSize)
             make.top.equalTo(self)
             make.left.equalTo(self).offset(20.0)
         }
         
-        self.label = UILabel()
-        self.label.textColor = UIColor.blackColor()
-        self.label.font = UIFont.systemFontOfSize(18)
-        self.label.textAlignment = .Left
-        self.label.numberOfLines = 0
-        self.label.text = text
-        self.addSubview(self.label)
-        self.label.snp_makeConstraints { make in
+        let label = UILabel()
+        label.textColor = UIColor.blackColor()
+        label.font = UIFont.systemFontOfSize(18)
+        label.textAlignment = .Left
+        label.numberOfLines = 0
+        label.text = text
+        addSubview(label)
+        label.snp_makeConstraints { make in
             make.top.equalTo(self)
-            make.left.equalTo(self.iconView.snp_right).offset(8.0)
-            make.right.equalTo(self).offset(-20.0)
+            make.left.equalTo(iconView.snp_right).offset(8.0)
+            make.right.equalTo(self).offset(-15.0)
+            make.height.greaterThanOrEqualTo(iconSize)
+        }
+        
+        self.setNeedsLayout()
+        self.layoutIfNeeded()
+        
+        self.snp_makeConstraints { make in
+            make.width.equalTo(frame.width)
+            make.height.equalTo(label)
         }
     }
     
