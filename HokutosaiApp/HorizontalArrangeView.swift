90cc03cdb372c4b1b8407219cef305f293a31fef
sejour <shuka.takakuma@gmail.com>
2016-05-07 14:24:05 +0900

    [modify] アイコンのタップ認識領域を拡大

--- HokutosaiApp/HorizontalArrangeView.swift
+++ HokutosaiApp/HorizontalArrangeView.swift
@@ -9,18 +9,21 @@
 import UIKit
 
 class HorizontalArrangeView: UIView {
-
-    convenience init(width: CGFloat, height: CGFloat, items: [UIView]) {
+    
+    convenience init(width: CGFloat, height: CGFloat, items: [UIView], horizontalMargin: CGFloat = 10.0) {
         self.init(size: CGSize(width: width, height: height), items: items)
     }
 
-    init(size: CGSize, items: [UIView]) {
+    init(size: CGSize, items: [UIView], horizontalMargin: CGFloat = 10.0) {
         super.init(frame: CGRect(origin: CGPointZero, size: size))
         
         let space = self.width / CGFloat(items.count + 1)
+        var width = space - horizontalMargin
+        if width < self.height { width = self.height }
         let centerY = self.height / 2.0
+        
         for i in 0 ..< items.count {
-            items[i].size = CGSize(width: self.height, height: self.height)
+            items[i].size = CGSize(width: width, height: self.height)
             items[i].center = CGPoint(x: space * CGFloat(i + 1), y: centerY)
             items[i].contentMode = .ScaleAspectFit
             self.addSubview(items[i])
