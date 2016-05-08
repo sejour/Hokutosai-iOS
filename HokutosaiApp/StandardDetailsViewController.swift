b72bf6ece8aa2c66c298d80aebf6f3139cb9a3d7
sejour <shuka.takakuma@gmail.com>
2016-05-09 04:24:34 +0900

    [implement] like関係を実装

--- HokutosaiApp/StandardDetailsViewController.swift
+++ HokutosaiApp/StandardDetailsViewController.swift
@@ -82,15 +82,72 @@ class StandardDetailsViewController<ModelType: StandardContentsData, TableViewCo
     }
     
     func like() {
+        guard let model = self.model else { return }
         
+        if let liked = model.dataLiked where liked {
+            self.likeIcon.image = SharedImage.largeGrayHertIcon
+            HokutosaiApi.DELETE(self.endpointLikes) { response in
+                guard let result = response.model else {
+                    self.presentViewController(ErrorAlert.Server.failureSendRequest(), animated: true, completion: nil)
+                    self.updateLikes(nil)
+                    return
+                }
+                
+                self.updateLikes(result)
+            }
+        }
+        else {
+            self.likeIcon.image = SharedImage.largeRedHertIcon
+            HokutosaiApi.POST(self.endpointLikes) { response in
+                guard let result = response.model else {
+                    self.presentViewController(ErrorAlert.Server.failureSendRequest(), animated: true, completion: nil)
+                    self.updateLikes(nil)
+                    return
+                }
+                
+                self.updateLikes(result)
+            }
+        }
     }
     
     func updateLikes(like: LikeResult?) {
+        if let like = like {
+            self.model?.dataLiked = like.liked
+            self.model?.dataLikesCount = like.likesCount
+            self.likesCountLabel.text = "いいね \(self.model?.dataLikesCount ?? 0)件"
+            self.tableViewController?.reloadData()
+            // もしself.eventがTopicEventであればTimetableのEventは更新されない -> Timetableを更新
+            self.tableViewController?.reloadData()
+        }
         
+        if let liked = self.model?.dataLiked where liked {
+            self.likeIcon.image = SharedImage.largeRedHertIcon
+        }
+        else {
+            self.likeIcon.image = SharedImage.grayHertIcon
+        }
     }
     
     func updateLike() {
-        
+        HokutosaiApi.GET(self.endpointDetails) { response in
+            guard response.isSuccess, let data = response.model else {
+                return
+            }
+            
+            // もしself.eventがTopicEventであればTimetableのEventは更新されない。
+            // 本来ならばTimetableを更新するべきだがTimetable詳細ビューを開くごとにTimetableを更新するのでは更新が頻繁になるため、ここはでは更新しない。 -> 整合性を犠牲にしている
+            self.model?.dataLiked = data.dataLiked
+            self.model?.dataLikesCount = data.dataLikesCount
+            self.likesCountLabel.text = "いいね \(self.model?.dataLikesCount ?? 0)件"
+            self.tableViewController?.reloadData()
+            
+            if let liked = self.model?.dataLiked where liked {
+                self.likeIcon.image = SharedImage.largeRedHertIcon
+            }
+            else {
+                self.likeIcon.image = SharedImage.grayHertIcon
+            }
+        }
     }
     
     func share() {
