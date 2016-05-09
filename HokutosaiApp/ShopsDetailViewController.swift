99925f442eb271ee6960cee5c0a54089d42e5769
Shuka Takakuma <shuka.takakuma@gmail.com>
2016-05-09 14:47:01 +0900

    [add] ShopsDetailViewController

---
+++ HokutosaiApp/ShopsDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ShopsDetailViewController.swift
+//  HokutosaiApp
+//
+//  Created by Shuka Takakuma on 2016/05/09.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import Foundation
+
+class ShopsDetailViewController: StandardDetailsViewController<DetailedShop, ShopsViewController> {
+        
+}
