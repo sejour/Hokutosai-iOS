6cf315b901209199ae38248c26807f6dbaa292ab
sejour <shuka.takakuma@gmail.com>
2016-05-11 04:50:45 +0900

    [implement] generateContentsの基本部分を実装

--- HokutosaiApp/NewsDetailViewController.swift
+++ HokutosaiApp/NewsDetailViewController.swift
@@ -30,12 +30,49 @@ class NewsDetailViewController: ContentsViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.generateContents()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func generateContents() {
+        //
+        self.insertSpace(10.0)
+        //
+        
+        // Title View
+        let title = self.article.title ?? "タイトル無し"
+        let titleView = TitleView(width: self.view.width, title: title, featured: self.article.isTopic)
+        self.addContentView(titleView)
+        
+        //
+        self.insertSpace(8.0)
+        //
+        
+        // Meta Label
+        let metaLabel = ArticleMetaLabel(width: self.view.width, relatedTitle: self.article.relatedTitle, target: self, action: #selector(NewsDetailViewController.tappedRelatedLink), datetime: self.article.datetime)
+        self.addContentView(metaLabel)
+        
+        //
+        self.insertSpace(1.0)
+        self.insertSeparator(20.0)
+        self.insertSpace(15.0)
+        //
+        
+        // Text
+        let textLabel = TextLabel(width: self.view.width, text: self.article.text)
+        self.addContentView(textLabel)
+        
+        //
+        self.insertSpace(20.0)
+        //
+    }
+    
+    func tappedRelatedLink() {
+        print("tapped")
     }
 
 }
