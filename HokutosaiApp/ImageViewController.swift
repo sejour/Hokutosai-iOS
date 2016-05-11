0890dd414872482c37e88425f0435a8a72943c43
sejour <shuka.takakuma@gmail.com>
2016-05-12 01:49:33 +0900

    [add] 画像の拡大機能を追加

--- HokutosaiApp/ImageViewController.swift
+++ HokutosaiApp/ImageViewController.swift
@@ -51,23 +51,41 @@ class ImageViewController: SlidePageViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    class Item: UIViewController {
+    class Item: UIViewController, UIScrollViewDelegate {
         
         private var imageView: UIImageView!
+        private var scrollView: UIScrollView!
         
         override func viewDidLoad() {
             super.viewDidLoad()
             
             self.view.backgroundColor = UIColor.blackColor()
             
-            self.imageView = UIImageView()
-            self.imageView.image = SharedImage.placeholderImage
-            self.view.addSubview(self.imageView)
-            self.imageView.snp_makeConstraints { make in
+            // ScrollView
+            self.scrollView = UIScrollView()
+            self.scrollView.delegate = self
+            self.scrollView.minimumZoomScale = 1.0
+            self.scrollView.maximumZoomScale = 4.0
+            self.scrollView.scrollEnabled = true
+            self.scrollView.showsHorizontalScrollIndicator = true
+            self.scrollView.showsVerticalScrollIndicator = true
+            self.view.addSubview(self.scrollView)
+            self.scrollView.snp_makeConstraints { make in
                 make.left.equalTo(self.view)
                 make.top.equalTo(self.view)
                 make.right.equalTo(self.view)
                 make.bottom.equalTo(self.view)
+            }
+            
+            // ImageView
+            self.imageView = UIImageView()
+            self.imageView.image = SharedImage.placeholderImage
+            self.scrollView.addSubview(self.imageView)
+            self.imageView.snp_makeConstraints { make in
+                make.left.equalTo(self.scrollView)
+                make.top.equalTo(self.scrollView)
+                make.width.equalTo(self.scrollView)
+                make.height.equalTo(self.scrollView)
             }
             
             self.imageView.contentMode = .ScaleAspectFit
@@ -77,6 +95,13 @@ class ImageViewController: SlidePageViewController {
             
             let tapGesture = UITapGestureRecognizer(target: self, action: #selector(Item.tapped(_:)))
             self.imageView.addGestureRecognizer(tapGesture)
+            
+            let doubleTapGesture = UITapGestureRecognizer(target: self, action: #selector(Item.doubleTapped(_:)))
+            doubleTapGesture.numberOfTapsRequired = 2
+            self.imageView.addGestureRecognizer(doubleTapGesture)
+            
+            // ダブルタップによる拡大を優先する
+            tapGesture.requireGestureRecognizerToFail(doubleTapGesture)
         }
         
         override func didReceiveMemoryWarning() {
@@ -91,6 +116,33 @@ class ImageViewController: SlidePageViewController {
         
         func tapped(sender: UITapGestureRecognizer) {
             self.switchTopBarVisible(true)
+        }
+        
+        func doubleTapped(gesture: UITapGestureRecognizer) {
+            if self.scrollView.zoomScale <= self.scrollView.minimumZoomScale {
+                self.hiddenTopBar(true, animated: true)
+                let rect = self.zoomRectForScale(self.scrollView.maximumZoomScale, center: gesture.locationInView(gesture.view))
+                self.scrollView.zoomToRect(rect, animated: true)
+            }
+            else {
+                self.scrollView.setZoomScale(self.scrollView.minimumZoomScale, animated: true)
+            }
+        }
+        
+        func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+            return self.imageView
+        }
+        
+        func zoomRectForScale(scale: CGFloat, center: CGPoint) -> CGRect {
+            // scale倍拡大する = 表示領域を1/scaleにする
+            let width = self.scrollView.width / scale
+            let height = self.scrollView.height / scale
+            
+            // 拡大領域の原点を求める
+            let x = center.x - (width / 2.0)
+            let y = center.y - (height / 2.0)
+        
+            return CGRect(x: x, y: y, width: width, height: height)
         }
         
     }
