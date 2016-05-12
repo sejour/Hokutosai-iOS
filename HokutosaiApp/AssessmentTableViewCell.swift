09599ed54be64d726720d496d948a1ed0d4d5d68
sejour <shuka.takakuma@gmail.com>
2016-05-13 04:49:19 +0900

    [modify] セルをタップできないように変更

--- HokutosaiApp/AssessmentTableViewCell.swift
+++ HokutosaiApp/AssessmentTableViewCell.swift
@@ -20,6 +20,8 @@ class AssessmentTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        self.selectionStyle = .None
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
