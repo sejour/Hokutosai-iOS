3ce6eb70d81f4d7c23872457ee76b22adb254ab6
sejour <shuka.takakuma@gmail.com>
2016-05-13 00:38:56 +0900

    [add] AssessmentTableViewCell

---
+++ HokutosaiApp/AssessmentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AssessmentTableViewCell.swift
+//  HokutosaiApp
+//
+//  Created by Shuka Takakuma on 2016/05/13.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import UIKit
+
+class AssessmentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
