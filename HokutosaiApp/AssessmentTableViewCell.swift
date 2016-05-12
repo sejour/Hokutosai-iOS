1d750bd72fd974ab06201ab07006ccb0c1c8b6d2
sejour <shuka.takakuma@gmail.com>
2016-05-13 03:24:01 +0900

    [implement] changeData

--- HokutosaiApp/AssessmentTableViewCell.swift
+++ HokutosaiApp/AssessmentTableViewCell.swift
@@ -15,6 +15,8 @@ class AssessmentTableViewCell: UITableViewCell {
     @IBOutlet weak var commentLabel: UILabel!
     @IBOutlet weak var datetimeLabel: UILabel!
     
+    var assessmentId: UInt!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -31,7 +33,25 @@ class AssessmentTableViewCell: UITableViewCell {
     }
     
     func changeData(assessment: Assessment) {
+        self.assessmentId = assessment.assessmentId
         
+        self.userNameLabel.text = assessment.user?.userName ?? "ゲスト"
+        
+        if let score = assessment.score where score >= 0 && score <= 5 {
+            self.scoreImageView.image = SharedImage.scoreImage[Int(score)]
+        }
+        else {
+            self.scoreImageView.image = SharedImage.scoreImage[0]
+        }
+        
+        self.commentLabel.text = assessment.comment ?? ""
+        
+        if let datetime = assessment.datetime {
+            self.datetimeLabel.text = NSDate.stringFromDate(datetime, format: "yyyy/MM/dd HH:mm")
+        }
+        else {
+            self.datetimeLabel.text = "投稿時間不明"
+        }
     }
     
 }
