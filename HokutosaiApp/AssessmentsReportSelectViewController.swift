21a277c3fef214dfadc98a1ae445d4a80680280f
sejour <shuka.takakuma@gmail.com>
2016-05-13 07:23:36 +0900

    [add] AssessmentsReportSelectViewController

---
+++ HokutosaiApp/AssessmentsReportSelectViewController.swift
@@ -0,0 +1,46 @@
+//
+//  AssessmentsReportSelectViewController.swift
+//  HokutosaiApp
+//
+//  Created by Shuka Takakuma on 2016/05/13.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import UIKit
+
+class AssessmentsReportSelectViewController: UITableViewController {
+
+    private let cellIdentifier = "AssessmentReport"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: self.cellIdentifier)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(self.cellIdentifier, forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+}
