6ac3dfb858b5dc195a3e99dad6a42470e4b0e828
sejour <shuka.takakuma@gmail.com>
2016-05-13 01:59:23 +0900

    [add] AssessmentsListViewController

---
+++ HokutosaiApp/AssessmentsListViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AssessmentsListViewController.swift
+//  HokutosaiApp
+//
+//  Created by Shuka Takakuma on 2016/05/13.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import UIKit
+
+class AssessmentsListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+}
