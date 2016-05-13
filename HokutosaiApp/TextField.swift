6f3a9ba3f8dce736ccc72ad21586e59332c1a41d
sejour <shuka.takakuma@gmail.com>
2016-05-14 05:17:06 +0900

    [add] 文字数制限機能を追加

--- HokutosaiApp/TextField.swift
+++ HokutosaiApp/TextField.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TextField: UIView {
+class TextField: UIView, UITextFieldDelegate {
     
     class Property {
         var defaultText: String?
@@ -16,9 +16,13 @@ class TextField: UIView {
         var font: UIFont = UIFont.systemFontOfSize(20.0)
         var textColor: UIColor = UIColor.blackColor()
         var alignment: NSTextAlignment = .Left
+        var characterLimit: UInt?
     }
     
     private static let viewHeight: CGFloat = 22.0
+    
+    private var _textField: UITextField!
+    private var characterLimit: UInt?
 
     convenience init(width: CGFloat, property: Property = Property()) {
         self.init(frame: CGRect(x: 0.0, y: 0.0, width: width, height: 0.0), property: property)
@@ -27,15 +31,20 @@ class TextField: UIView {
     init(frame: CGRect, property: Property = Property()) {
         super.init(frame: frame)
         
-        let textField = UITextField()
-        textField.font = property.font
-        textField.textColor = property.textColor
-        textField.textAlignment = property.alignment
-        textField.text = property.defaultText
-        textField.placeholder = property.placeholder
+        self.characterLimit = property.characterLimit
         
-        self.addSubview(textField)
-        textField.snp_makeConstraints { make in
+        self._textField = UITextField()
+        self._textField.font = property.font
+        self._textField.textColor = property.textColor
+        self._textField.textAlignment = property.alignment
+        self._textField.text = property.defaultText
+        self._textField.placeholder = property.placeholder
+        self._textField.delegate = self
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(TextField.textFieldDidChange(_:)), name: UITextFieldTextDidChangeNotification, object: self._textField)
+        
+        self.addSubview(self._textField)
+        self._textField.snp_makeConstraints { make in
             make.left.equalTo(self).offset(20.0)
             make.right.equalTo(self).offset(-20.0)
             make.top.equalTo(self)
@@ -50,6 +59,44 @@ class TextField: UIView {
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    var text: String? {
+        get { return self._textField.text }
+        set { self._textField.text = newValue }
+    }
+    
+    private var previousText = ""
+    private var lastReplaceRange: NSRange!
+    private var lastReplacementString = ""
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        self.previousText = textField.text!
+        self.lastReplaceRange = range
+        self.lastReplacementString = string
+        
+        return true
+    }
+    
+    func textFieldDidChange(notification: NSNotification) {
+        guard let characterLimit = self.characterLimit, let textField = notification.object as? UITextField where textField.markedTextRange == nil else {
+            return
+        }
+        
+        let maxLength = Int(characterLimit)
+        
+        if textField.text!.characters.count > maxLength {
+            let offset = maxLength - textField.text!.characters.count
+            let replacementString = (self.lastReplacementString as NSString).substringToIndex(self.lastReplacementString.characters.count + offset)
+            let text = (previousText as NSString).stringByReplacingCharactersInRange(lastReplaceRange, withString: replacementString)
+            let position = textField.positionFromPosition(textField.selectedTextRange!.start, offset: offset)!
+            let selectedTextRange = textField.textRangeFromPosition(position, toPosition: position)
+            
+            textField.text = text
+            textField.selectedTextRange = selectedTextRange
+        }
+        
+        return
     }
 
 }
