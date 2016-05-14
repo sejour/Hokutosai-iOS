636e49194f6b8401ded3a94ea5a4fc3ecdd3d4a0
sejour <shuka.takakuma@gmail.com>
2016-05-15 08:10:08 +0900

    [add] OthersItem model

---
+++ HokutosaiApp/OthersItem.swift
@@ -0,0 +1,21 @@
+//
+//  OthersItem.swift
+//  HokutosaiApp
+//
+//  Created by Shuka Takakuma on 2016/05/15.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import Foundation
+
+class OthersItem {
+    
+    let title: String
+    let url: String?
+    
+    init(title: String, url: String?) {
+        self.title = title
+        self.url = url
+    }
+    
+}
