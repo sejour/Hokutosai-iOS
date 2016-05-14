8936a2b1b18dba34461c25e2004af76c073160bb
sejour <shuka.takakuma@gmail.com>
2016-05-14 21:50:26 +0900

    [add] Stringが空白文字かどうかを調べるメソッドを追加

---
+++ HokutosaiApp/String.swift
@@ -0,0 +1,17 @@
+//
+//  String.swift
+//  HokutosaiApp
+//
+//  Created by Shuka Takakuma on 2016/05/14.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import UIKit
+
+extension String {
+    
+    var isBlank: Bool {
+        return stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet()).isEmpty
+    }
+    
+}
