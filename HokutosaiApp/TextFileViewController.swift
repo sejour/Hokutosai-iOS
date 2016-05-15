57ee2c179ac61cdb562f6d443917032908098141
sejour <shuka.takakuma@gmail.com>
2016-05-15 17:52:47 +0900

    [add] TextFileViewController

---
+++ HokutosaiApp/TextFileViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TextFileViewController.swift
+//  HokutosaiApp
+//
+//  Created by Shuka Takakuma on 2016/05/15.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import UIKit
+
+class TextFileViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    
+    var fileName: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let name = self.fileName {
+            let path = NSBundle.mainBundle().pathForResource(name, ofType: "txt")!
+            if let data = NSData(contentsOfFile: path){
+                self.textView.text = String(NSString(data: data, encoding: NSUTF8StringEncoding)!)
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
